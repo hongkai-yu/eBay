7036b019b79ad4562110c3bc8d1e7e9b0c8a3252
Hongkai Yu <hongkaiyu1999@gmail.com>
2020-06-08 16:48:46 -0700

    try to figure out the labels

--- camera.R
+++ camera.R
@@ -1,5 +1,5 @@
 # Author : Hongkai Yu
-# Date   : 6 June, 2020
+# Date   : 8 June, 2020
 # install.packages("tidyverse")
 library(tidyverse)
 camera <- read_tsv("camera.txt", col_names = FALSE, col_types = 
@@ -11,7 +11,7 @@ camera <- read_tsv("camera.txt", col_names = FALSE, col_types =
                        X5 = col_double(),
                        X6 = col_character(),
                        X7 = col_double(),
-                       X8 = col_double(),
+                       X8 = col_integer(),
                        X9 = col_character(),
                        X10 = col_double(),
                        X11 = col_integer(),
@@ -24,32 +24,59 @@ camera <- read_tsv("camera.txt", col_names = FALSE, col_types =
                        X17 = col_datetime(format = '%Y%m%d%H%M%S')
                      ))
 
-camera_named <- camera %>%
+camera_named <- camera %>% # names with "_maybe" are guesses 
   mutate(X15 = parse_datetime(X15, format ='%Y%m%d%H%M%S', na = c("200012:0000000", "20000."))) %>%
   rename(bid_id = X1, 
          item_id = X2, 
          product = X3, 
          model = X4, 
-         trade_price = X5, 
+         trade_price_maybe = X5,    # related to the highest bid, but not always the same
          seller = X6, 
-         X7 = X7, 
+         reserve_price = X7, 
          X8 = X8, 
          bidder = X9, 
          bid_price = X10, 
+         X11 = X11,
+         X12 = X12,
+         X13 = X13,
+         X14 = X14,
          bid_time = X15,
          post_time = X16,
          complete_time = X17)
 View(camera_named)
 
-camera_named %>%
-  group_by()
-  summarise(X11_x = sum(), group_by(product))
+# X1, X2, X3, X4, X6, X9, X10 are clear
 
-camera_named %>%
+# --- evidence related to X15, X16, X17 (datetime) --- #
+table(camera_named$bid_time > camera_named$post_time) # always true
+table(camera_named$bid_time > camera_named$complete_time) # always false
+
+# --- evidence related to X5, X7 (prices) --- #
+table(camera_named$bid_price >= camera_named$reserve_price) # not always true, but all falses are due to invalid bids
+camera_named %>% filter(bid_price < reserve_price) %>% View
+
+summary <- camera_named %>%
+  group_by(product) %>%
+  summarise(highest_bid = max(bid_price),
+            trade_price = mean(trade_price_maybe))
+table(summary$trade_price == summary$highest_bid) # not always true
+summary %>% filter(trade_price != highest_bid) %>% View 
+# some are zeros (could be due to invalid bids), but for other inequalities it could be higher or lower (so they aren't due to second auction mechanism), which are confusing
+
+  
+# --- evidence related to X11 - X14 (logical values) --- #
+# it seems that X11, X12, X13, X14 are attributes to the product, since the values are the same for each product.
+
+table(camera_named$X12) 
+table(camera_named$X13) 
+table(camera_named$X14) 
+
+table(camera_named$X11) # not only has 0 and 1, can be as high as 30
+
+camera_named %>% # investigate the 30 see if there is anything weird about it
   filter(X11 == 30) %>%
-  select(seller)
+  select(seller)  # sam@xprt.net
 
 camera_named %>%
   filter(seller == 'sam@xprt.net') %>%
-  View()
-
+  View # nothing very peculiar about it
